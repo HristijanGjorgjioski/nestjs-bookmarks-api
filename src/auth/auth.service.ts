ed4fa980edef12368e277c311eaebd3900397b1a
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-09-19 19:04:00 +0200

    dev: created login and register logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,54 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from "argon2";
 import { User } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  async login({ email, password }: AuthDto): Promise<User> {
-    const hash = await argon.hash(password);
-    const user = await this.prisma.user.create({
-      data: {
+
+  // Login
+  async login({ email, password }: AuthDto): Promise<{
+    user: User;
+    message: string;
+  }> {
+    const user = await this.prisma.user.findUnique({
+      where: {
         email,
-        hash,
       },
     });
 
-    delete user.hash;
-    return user;
+    if (!user) throw new ForbiddenException("Credentials incorrect");
+
+    const pwMatches = await argon.verify(user.hash, password);
+    if (!pwMatches) throw new ForbiddenException("Credentials incorrect");
+
+    return { user, message: "Successfully logged in" };
   }
 
-  signup() {
-    return "Signup";
+  // Signup/Register
+  async signup({
+    email,
+    password,
+  }: AuthDto): Promise<{ user: User; message: string }> {
+    const hash = await argon.hash(password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email,
+          hash,
+        },
+      });
+      return { user, message: "Successfully registered" };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === "P2002") {
+          throw new ForbiddenException("Credentials taken");
+        }
+      }
+      throw error;
+    }
   }
 }
