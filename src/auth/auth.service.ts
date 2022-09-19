fec9fb60c6c419ff7cd290e6f6fcf625fa126450
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-09-19 18:49:31 +0200

    dev: relation between dbs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from "argon2";
+import { User } from "@prisma/client";
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  login(dto: any) {
-    console.log(dto);
+  async login({ email, password }: AuthDto): Promise<User> {
+    const hash = await argon.hash(password);
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        hash,
+      },
+    });
+
+    delete user.hash;
+    return user;
   }
 
   signup() {
