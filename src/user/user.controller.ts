93f8d42b23b293140b5e726f320aa1c7d09335cd
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-09-19 23:05:49 +0200

    dev: implementing nest-guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
+import { JwtGuard } from "src/auth/guard";
 
 @Controller("users")
 export class UserController {
+  constructor() {}
+  @UseGuards(JwtGuard)
   @Get("me")
   getMe() {
-    return "user info";
+    return "hoho";
   }
 }
