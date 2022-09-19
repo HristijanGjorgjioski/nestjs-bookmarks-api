399427af7e42e0fe3dee032b0895511456dbe917
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2022-09-19 20:22:01 +0200

    dev: implementing jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,36 @@
 import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { JwtService } from "@nestjs/jwt";
 import { AuthDto } from "./dto";
 import * as argon from "argon2";
-import { User } from "@prisma/client";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwt: JwtService) {}
+
+  // Sign JWT Token
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const access_token = await this.jwt.signAsync(payload, {
+      expiresIn: "1h",
+      secret: process.env.JWT_SECRET,
+    });
+
+    return {
+      access_token,
+    };
+  }
 
   // Login
-  async login({ email, password }: AuthDto): Promise<{
-    user: User;
-    message: string;
-  }> {
+  async login({ email, password }: AuthDto): Promise<{ access_token: string }> {
     const user = await this.prisma.user.findUnique({
       where: {
         email,
@@ -25,14 +42,14 @@ export class AuthService {
     const pwMatches = await argon.verify(user.hash, password);
     if (!pwMatches) throw new ForbiddenException("Credentials incorrect");
 
-    return { user, message: "Successfully logged in" };
+    return this.signToken(user.id, email);
   }
 
   // Signup/Register
   async signup({
     email,
     password,
-  }: AuthDto): Promise<{ user: User; message: string }> {
+  }: AuthDto): Promise<{ access_token: string }> {
     const hash = await argon.hash(password);
     try {
       const user = await this.prisma.user.create({
@@ -41,7 +58,7 @@ export class AuthService {
           hash,
         },
       });
-      return { user, message: "Successfully registered" };
+      return this.signToken(user.id, email);
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === "P2002") {
